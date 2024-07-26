6a704938217bece098ff948ff4bc86cc24f7630f
Carl Michael Skog <cm@xn--ger-pla.com>
2024-07-26 15:03:35 +0200

    Unique name for the script
    Name the script runtmux-<session name>, instead of just runtmux.
    Set meta.mainProgram to this name.
    A bit more practical, if we need more than one session.
    In build.nix, add an extra runtmux session, and create a desktop file
    that uses the meta.mainProgram.
    Add a test for checking that the tmux-session value actually ends up
    being a regular file.

--- build.nix
+++ build.nix
@@ -2,51 +2,92 @@
   pkgs ? import <nixpkgs> {}
 }
 :
+(
+  builtins.concatMap
 
-pkgs.callPackage
-./.
-{
+  (runtmux:
+    [
+      runtmux
+      (
+        pkgs.makeDesktopItem
+        {
+          name = "uxterm-runtmux-${runtmux.session-name}";
+          desktopName = "Test runtmux(Session ${runtmux.session-name}) in UXterm";
+          exec = "${pkgs.xterm}/bin/uxterm -e \"${pkgs.lib.meta.getExe runtmux}\"";
+          icon = "org.xfce.terminalemulator";
+        }
+      )
+    ]
+  )
 
-  session-name =
-    "A session";
-
-  tmux-config =
-    pkgs.writeText
-    "tmux.conf"
-    ''
-    set-option -g prefix C-a
-    unbind-key C-b
-    bind-key C-a send-prefix
-
-    set-option -g status-left "[#S] "
-    set-option -g status-left-length 20
-    set-option -g status-right " %a %Y-%m-%d %H:%M %Z(%z)"
-
-    set-option -g status-style "fg=#ffff00 bold"
-    set-option -ag status-style "bg=#0000ff"
-
-    #
-    # START Ctrl keys
-    #
-
-    # Start editor
-    #
-    bind-key	-T prefix	C-e \
-      new-window -n Vim ${pkgs.vim}/bin/vim
-    '';
-
-  session-progs =
   [
-    [
-      "Edit"
-      /Dev
-      (pkgs.lib.meta.getExe pkgs.vim)
-    ]
+    (
+    pkgs.callPackage
+    ./.
+    {
 
-    [
-      "/Dev"
-      /Dev
-    ]
-  ];
+      session-name =
+        "A session";
 
-}
+      tmux-config =
+        pkgs.writeText
+        "tmux.conf"
+        ''
+        set-option -g prefix C-a
+        unbind-key C-b
+        bind-key C-a send-prefix
+
+        set-option -g status-left "[#S] "
+        set-option -g status-left-length 20
+        set-option -g status-right " %a %Y-%m-%d %H:%M %Z(%z)"
+
+        set-option -g status-style "fg=#ffff00 bold"
+        set-option -ag status-style "bg=#0000ff"
+
+        #
+        # START Ctrl keys
+        #
+
+        # Start editor
+        #
+        bind-key	-T prefix	C-e \
+          new-window -n Vim ${pkgs.vim}/bin/vim
+        '';
+
+      session-progs =
+      [
+        [
+          "Edit"
+          /Dev
+          (pkgs.lib.meta.getExe pkgs.vim)
+        ]
+
+        [
+          "/Dev"
+          /Dev
+        ]
+      ];
+
+    }
+    )
+    (
+      pkgs.callPackage
+      ./.
+      {
+
+        session-name =
+          "Ranger";
+
+        session-progs =
+        [
+          [
+            "Ranger"
+            /Dev
+            (pkgs.lib.meta.getExe pkgs.ranger)
+          ]
+        ];
+
+      }
+    )
+  ]
+)
--- default.nix
+++ default.nix
@@ -14,9 +14,6 @@
 assert builtins.isString session-name;
 assert (builtins.stringLength session-name) > 0;
 
-# We should do some checks on tmux-config:
-# It must result in a file with the tmux configuration(if not null)
-
 assert builtins.isList session-progs;
 assert
   builtins.all
@@ -37,78 +34,88 @@ assert
   )
   session-progs;
 
-writeTextFile {
-  name = "runtmux";
-  executable = true;
-  destination = "/bin/runtmux";
-  derivationArgs =
-  {
-    passthru =
+writeTextFile
+(
+  let
+    script = "runtmux-${session-name}";
+  in
     {
-      inherit session-name;
-    };
-  };
-  text =
-    (
-      let
-        tmux-prog = lib.meta.getExe tmux;
-      in
-        ''
-        #! ${runtimeShell}
+      name = "runtmux";
+      executable = true;
+      destination = "/bin/${script}";
+      text =
+        (
+          let
+            tmux-prog = lib.meta.getExe tmux;
+          in
+            ''
+            #! ${runtimeShell}
 
-        set \
-          -o errexit \
-          -o nounset \
-          -o pipefail
-        shopt -s shift_verbose
+            set \
+              -o errexit \
+              -o nounset \
+              -o pipefail
+            shopt -s shift_verbose
 
-        if ${tmux-prog} has-session -t "${session-name}" >& /dev/null
-        then
-          exec ${tmux-prog} \
-            attach-session -t "${session-name}"
-        else
-          exec ${tmux-prog} ${
-                             if tmux-config != null
-                             then
-                               "-f '${tmux-config}'"
-                             else
-                               ""
-                            } \
-            new-session -s "${session-name}" \
-          ${
-            builtins.toString
-            (
-              let
-                unpruned =
-                  builtins.concatLists
-                    (builtins.map
-                      (p:
-                        [
-                          "new-window -d"
+            if ${tmux-prog} has-session -t "${session-name}" >& /dev/null
+            then
+              exec ${tmux-prog} \
+                attach-session -t "${session-name}"
+            else
+              exec ${tmux-prog} ${
+                                  if tmux-config != null
+                                  then
+                                    assert builtins.readFileType tmux-config == "regular";
+                                      "-f '${tmux-config}'"
+                                  else
+                                    ""
+                                } \
+                new-session -s "${session-name}" \
+              ${
+                builtins.toString
+                (
+                  let
+                    unpruned =
+                      builtins.concatLists
+                        (builtins.map
+                          (p:
+                            [
+                              "new-window -d"
 
-                          (
-                            "-n "
-                            + (builtins.elemAt p 0)
-                            + " -c "
-                            + (builtins.toString (builtins.elemAt p 1))
-                            + (
-                               if (builtins.length p) == 3
-                               then
-                                 " " + (builtins.elemAt p 2)
-                               else
-                                 ""
+                              (
+                                "-n "
+                                + (builtins.elemAt p 0)
+                                + " -c "
+                                + (builtins.toString (builtins.elemAt p 1))
+                                + (
+                                   if (builtins.length p) == 3
+                                   then
+                                     " " + (builtins.elemAt p 2)
+                                   else
+                                     ""
+                                  )
                               )
-                          )
 
-                          "\\;"
-                        ]
-                      )
-                      session-progs
-                    );
-              in
-                lib.lists.drop 1 (lib.lists.take ((builtins.length unpruned) - 1) unpruned)
-            )
-          }
-        fi''
-    );
-}
+                              "\\;"
+                            ]
+                          )
+                          session-progs
+                        );
+                  in
+                    lib.lists.drop 1 (lib.lists.take ((builtins.length unpruned) - 1) unpruned)
+                )
+              }
+            fi''
+        );
+
+      derivationArgs =
+      {
+        passthru =
+        {
+          inherit session-name;
+        };
+      };
+
+      meta.mainProgram = script;
+    }
+)
